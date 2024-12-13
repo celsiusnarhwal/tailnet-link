bf0a009c18e959ddad1593c795158a48ccd984e7
celsius narhwal <hello@celsiusnarhwal.dev>
2024-12-13 12:10:57 -0500

    Update tscmd.py

--- tscmd/tscmd/tscmd.py
+++ tscmd/tscmd/tscmd.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 
 def main():
-    print((sys.argv[1:] == "logout" and os.getenv("_TAILNET_LINK_DO_NOT_DISCONNECT")))
-    if not (sys.argv[1:] == "logout" and os.getenv("_TAILNET_LINK_DO_NOT_DISCONNECT")):
+    print((sys.argv[1] == "logout" and os.getenv("_TAILNET_LINK_DO_NOT_DISCONNECT")))
+    if not (sys.argv[1] == "logout" and os.getenv("_TAILNET_LINK_DO_NOT_DISCONNECT")):
         if sys.platform == "win32":
             tailscale = [Path("C:/Program Files/Tailscale/tailscale.exe")]
         else:
