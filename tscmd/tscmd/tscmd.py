7bfc2a9d4ba2b76f0027c30764d6bd69e902e583
celsius narhwal <hello@celsiusnarhwal.dev>
2024-12-13 11:22:14 -0500

    Update tscmd.py

--- tscmd/tscmd/tscmd.py
+++ tscmd/tscmd/tscmd.py
@@ -1,12 +1,14 @@
+import os
 import subprocess
 import sys
 from pathlib import Path
 
 
 def main():
-    if sys.platform == "win32":
-        tailscale = [Path("C:/Program Files/Tailscale/tailscale.exe")]
-    else:
-        tailscale = ["sudo", "tailscale"]
+    if not (sys.argv[1:] == "logout" and os.getenv("_TAILNET_LINK_DO_NOT_DISCONNECT")):
+        if sys.platform == "win32":
+            tailscale = [Path("C:/Program Files/Tailscale/tailscale.exe")]
+        else:
+            tailscale = ["sudo", "tailscale"]
 
-    subprocess.run(tailscale + sys.argv[1:])
+        subprocess.run(tailscale + sys.argv[1:])
