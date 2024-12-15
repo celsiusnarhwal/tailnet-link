3b86493cff2caf1677cbaed1fc9003035356615a
celsius narhwal <hello@celsiusnarhwal.dev>
2024-12-15 11:16:37 -0500

    Update tscmd.py

--- tscmd/tscmd/tscmd.py
+++ tscmd/tscmd/tscmd.py
@@ -5,10 +5,9 @@ from pathlib import Path
 
 
 def main():
-    if not (sys.argv[1] == "logout" and os.getenv("_TAILNET_LINK_DO_NOT_DISCONNECT")):
-        if sys.platform == "win32":
-            tailscale = [Path("C:/Program Files/Tailscale/tailscale.exe")]
-        else:
-            tailscale = ["sudo", "tailscale"]
+    if sys.platform == "win32":
+        tailscale = [Path("C:/Program Files/Tailscale/tailscale.exe")]
+    else:
+        tailscale = ["sudo", Path(os.getenv("_TAILNET_LINK_TAILSCALE_BIN_PATH"))]
 
-        subprocess.run(tailscale + sys.argv[1:])
+    subprocess.run(tailscale + sys.argv[1:])
